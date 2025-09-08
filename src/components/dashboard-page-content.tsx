55a8c7caa47a6a76802ce1ed909a4a315613b21b
Rajat Sethi <rajatsethi.sethi7@gmail.com>
2025-09-08 10:14:12 +0000

    this here i guess we need top padding (_for element div_)

--- src/components/dashboard-page-content.tsx
+++ src/components/dashboard-page-content.tsx
@@ -558,7 +558,7 @@ export function DashboardPageContent() {
   }
 
   return (
-     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+     <main className="grid flex-1 items-start gap-8 p-4 sm:px-6 md:p-8">
         <div className="flex items-center">
             <h1 className="text-2xl font-semibold tracking-tight">{selectedProject.name}</h1>
             <div className="ml-auto flex items-center gap-2">
